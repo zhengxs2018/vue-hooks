6b66e74a5f7359159a11a61b6035382db61de224
zhengxs <zhengxs2018@gmail.com>
2020-10-03 18:57:38 +0800

    chore(package:vue-hooks): 修改打包配置

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,11 +1,24 @@
 export default {
-  // entry: './src/index.ts',
   esm: 'rollup',
   cjs: 'rollup',
   umd: {
-    minFile: true
+    minFile: true,
+    globals: {
+      'axios': 'axios',
+      'better-mock': 'Mock',
+      'vue': 'Vue',
+      "path-to-regexp": "pathToRegexp",
+      'tslib': 'window',
+    }
   },
   pkgs: ['vue-hooks'],
+  extraExternals: [
+    '@vue/reactivity',
+    "axios",
+    "better-mock",
+    "path-to-regexp",
+    'vue'
+  ],
   extraBabelPlugins: [
     'transform-async-to-promises'
   ]
