4e2b99cc67cc8882ce2017340c0000b6ee25303b
zhengxs <zhengxs2018@gmail.com>
2020-10-28 10:44:03 +0800

    WIP: 修改 pick 函数的类型定义

--- packages/vue-hooks/src/utils.ts
+++ packages/vue-hooks/src/utils.ts
@@ -2,9 +2,23 @@ export const noop = () => void 0
 
 export const hasOwn = Object.prototype.hasOwnProperty
 
-export function pick<T extends object, K extends (keyof T)[]>(data: T, keys: K): Partial<T> {
-  return keys.reduce((obj, key) => {
-    obj[key] = data[key]
-    return obj
-  }, {} as Partial<T>)
+/**
+ *
+ * @todo 这里的类型这么处理？
+ *
+ * @param data
+ * @param keys
+ */
+export function pick<T extends object, K extends Array<string | symbol | number> = Array<keyof T>>(
+  source: T,
+  keys: K
+): Partial<T> {
+  const target: Partial<T> = {}
+
+  keys.forEach((key) => {
+    // @ts-ignore
+    target[key] = source[key]
+  })
+
+  return target
 }
